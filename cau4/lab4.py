ac476dd494b2d0740199c2c232aa01628919c64c
Duocnp <nguyenphungduoc96@gmail.com>
2023-06-12 15:56:32 +0700

    test

---
+++ cau4/lab4.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import meraki_info
+requests.packages.urllib3.disable_warnings()
+baseUrl=meraki_info.URL
+API_key=meraki_info.API
+org_id = "681155"
+def get_org():
+    url = baseUrl + f"/organizations/{org_id}/inventory/devices"
+    header = {
+        "X-Cisco-Meraki-API-Key" : API_key
+    }
+    response= requests.get(url,headers=header,verify=False)
+    data=response.json()
+    parse_data=json.dumps(data,indent=4)
+    # print(data)
+    # print(type(data))
+    device_null=[]
+    for i in data:
+        if i['networkId']==None:
+            device_null.append(i)
+            
+    print(json.dumps(device_null, indent=4)) 
+    
+if __name__ == "__main__":
+    get_org()
---
+++ cau4/meraki_info.py
@@ -0,0 +1,2 @@
+URL = "https://api.meraki.com/api/v1"
+API = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
